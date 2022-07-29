4e625c11a00c1ab3b5a0dedc6ab9872060c58ee3
Isak <isak.shamun@gmail.com>
2022-07-29 15:00:01 +0200

    1.1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,12 +3,18 @@ const Header=(props)=>{
   <h1>{props.course}</h1>
   )
 }  
+
+const Part=(props)=>{
+  return(
+    <p>{props.partnbr} {props.exnbr}</p>
+  )
+}
 const Content=(props)=>{
     return(
     <>
-    <p>{props.part[0]} {props.exercises[0]}</p>
-    <p>{props.part[1]} {props.exercises[1]}</p>
-    <p>{props.part[2]} {props.exercises[2]}</p>
+    <Part partnbr={props.part[0]} exnbr={props.exercises[0]}/>
+    <Part partnbr={props.part[1]} exnbr={props.exercises[1]}/>
+    <Part partnbr={props.part[2]} exnbr={props.exercises[2]}/>
     </>
     )
   }
